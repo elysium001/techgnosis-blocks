fe7072b3ac7861b4b24d85ad5385fdb416d80b00
Omar <valis452@gmail.com>
2020-12-30 15:09:45 -0700

    adding carousel

--- gnosis-blocks/build/index.asset.php
+++ gnosis-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'be334c326cdf954b4481a2e8f3d041dc');
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '6c7ad206d5e94ba915011b6bba9b8760');
